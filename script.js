19f8a60d305778c63c503b2d3173a61b6865b5d1
Trevor Mearns <spiral272@gmail.com>
2021-05-21 10:12:38 +0900

    ready for code review approval

--- script.js
+++ script.js
@@ -3,117 +3,141 @@ const amountInput = document.querySelector('#amount');
 const dateInput = document.querySelector('#date');
 const storeInput = document.querySelector('#store');
 const table = document.querySelector('#expensetable');
+let expenseArray = JSON.parse(localStorage.getItem('expenseArray')) || [];
 
+function drawTable(e) {
+  e.preventDefault();
+  if (
+    !expenseInput.value ||
+    !amountInput.value ||
+    !dateInput.value ||
+    !storeInput.value
+  ) {
+    alert('Please fill out all input fields before submitting. ');
+    return;
+  }
 
-function getExpense() {
-    const expense = expenseInput.value;
-    return expense;
+  const newExpense = {
+    id: Date.now(),
+    item: expenseInput.value,
+    amount: amountInput.value,
+    date: `${monthToCalendar(parseMonth(getDate()))} ${parseDate(
+      getDate()
+    )}, ${parseYear(getDate())}`,
+    location: storeInput.value
+  };
+
+  addNewExpense(newExpense);
+  // document.getElementById('expenseForm').reset();
 }
 
-function getAmount() {
-    const amount = `$${parseFloat(amountInput.value).toFixed(2)}`;
-    return amount;
+function addNewExpense(expense) {
+  buildTable(expense);
+  expenseArray.push(expense);
+  pushToLocalStorage(expenseArray);
 }
+
+function pushToLocalStorage(array) {
+  localStorage.setItem('expenseArray', JSON.stringify(array));
+}
+document.querySelector('#submitbutton').addEventListener('click', drawTable);
 
 function getDate() {
-    const date = dateInput.value;
-    return date;
+  const date = dateInput.value;
+  return date;
 }
 
 function parseDate(date) {
-    const day = date.substr(8, 2);
-    return day;
+  const day = date.substr(8, 2);
+  return day;
 }
 
 function parseMonth(date) {
-    const month = parseInt(date.substr(5, 2)) - 1;
-    return month;
+  const month = parseInt(date.substr(5, 2)) - 1;
+  return month;
 }
 
 function monthToCalendar(month) {
-    const months = [
-        'January',
-        'February',
-        'March',
-        'April',
-        'May',
-        'June',
-        'July',
-        'August',
-        'September',
-        'October',
-        'November',
-        'December'
-    ]
-    return months[month];
+  const months = [
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+    'July',
+    'August',
+    'September',
+    'October',
+    'November',
+    'December'
+  ];
+  return months[month];
 }
 
 function parseYear(date) {
-    const year = date.substr(0, 4);
-    return year;
-}
-
-function getStore() {
-    const store = storeInput.value;
-    return store;
+  const year = date.substr(0, 4);
+  return year;
 }
 
 function createTableData(item) {
-    const tableData = document.createElement('td');
-    tableData.appendChild(document.createTextNode(item));
-    return tableData;
+  const tableData = document.createElement('td');
+  tableData.appendChild(document.createTextNode(item));
+  return tableData;
 }
 
 function createTableRow() {
-    const tableRow = document.createElement('tr');
-    tableRow.classList.add('row');
-    return tableRow;
+  const tableRow = document.createElement('tr');
+  tableRow.classList.add('row');
+  return tableRow;
 }
 
-function createDeleteButton() {
-    const deleteButton = document.createElement('button');
-    deleteButton.classList = 'delete';
-    deleteButton.appendChild(document.createTextNode('Remove'));
-    return deleteButton;
+function createDeleteButton(expense) {
+  const deleteButton = document.createElement('button');
+  deleteButton.classList = 'delete';
+  deleteButton.appendChild(document.createTextNode('Remove'));
+  deleteButton.addEventListener('click', (e) => {
+    e.preventDefault();
+    deleteRow(deleteButton, expense.id);
+  });
+  return deleteButton;
 }
 
-function deleteRow(element) {
-    if (element.target.classList.contains('delete')) {
-        element.target.closest('tr').remove();
+function deleteRow(deleteButton, id) {
+  deleteButton.parentElement.parentElement.remove();
+  deleteLocalExpenses(id);
+}
+
+function deleteLocalExpenses(id) {
+  for (let i = 0; i < expenseArray.length; i++) {
+    if (expenseArray[i].id === id) {
+      expenseArray = expenseArray.filter((item) => {
+        return item.id !== id;
+      });
+      pushToLocalStorage(expenseArray);
     }
+  }
 }
 
-function buildTable(expense, amount, date, store) {
-    const row = createTableRow();
-    const deleteRow = document.createElement('td');
-    deleteRow.appendChild(createDeleteButton());
-    row.appendChild(createTableData(expense));
-    row.appendChild(createTableData(amount));
-    row.appendChild(createTableData(date));
-    row.appendChild(createTableData(store));
-    row.appendChild(deleteRow);
-    table.appendChild(row);
+function buildTable(expense) {
+  const row = createTableRow();
+
+  row.appendChild(createTableData(expense.item));
+  row.appendChild(createTableData(expense.amount));
+  row.appendChild(createTableData(expense.date));
+  row.appendChild(createTableData(expense.location));
+  const deleteRow = document.createElement('td');
+  const deleteButton = createDeleteButton(expense);
+  deleteRow.appendChild(deleteButton);
+  row.appendChild(deleteRow);
+
+  table.appendChild(row);
+  console.log('deleteButton: ', deleteButton.parentElement.parentElement);
 }
 
-function drawTable() {
-    if (getExpense() === '' || getAmount() === `$${NaN}` || getDate() == '') {
-        return;
-    }
-    const expense = getExpense();
-    const amount = getAmount();
-    const date = `${monthToCalendar(parseMonth(getDate()))} ${parseDate(getDate())}, ${parseYear(getDate())}`;
-    const store = getStore();
-    buildTable(expense, amount, date, store);
-    expenseInput.value = '';
-    amountInput.value = '';
-    dateInput.value = '';
-    storeInput.value = '';
-}
-
-document.querySelector('#submitbutton').addEventListener('click', drawTable);
-document.querySelector('#expenseform').addEventListener('keydown', event => {
-    if (event.keyCode === 13) {
-        drawTable();
-    }
+window.addEventListener('load', (e) => {
+  e.preventDefault();
+  expenseArray.forEach((expense) => {
+    buildTable(expense);
+  });
 });
-table.addEventListener('click', deleteRow);
